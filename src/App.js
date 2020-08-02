1bff44ebd7c23adc4b1f632eb31cb1a0fc8519bb
André Luiz <andregomesenator@gmail.com>
2020-08-02 19:41:41 -0300

    Concluido o Header.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Navbar, Container} from 'rbx';
 import './App.scss';
-import Header from './components/header';
+import logoImage from '../src/assets/images/logo.png';
+import '../src/styles/header.scss';
 
 
 const App = () => (
-  <Fragment>
-    <Header />
-  </Fragment>
+  <Navbar>
+            <Container>
+                <Navbar.Brand>
+                  <img src={logoImage} />
+                    <Navbar.Burger
+                        className="navbar-burger burger"
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbar-menu">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </Navbar.Burger>
+                </Navbar.Brand>
+
+                <Navbar.Menu id="navbar-menu">
+                    <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
+                        One Item
+                    </Navbar.Segment>
+                </Navbar.Menu>
+            </Container>
+        </Navbar>
 
 )
 
